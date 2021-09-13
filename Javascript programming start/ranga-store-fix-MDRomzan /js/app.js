01284f52bc4f254549511d4830fcd477ac1bc389
MDRomzan <mdromjan483@gmail.com>
2021-09-13 09:35:37 +0600

    sd

--- Javascript programming start/ranga-store-fix-MDRomzan /js/app.js
+++ Javascript programming start/ranga-store-fix-MDRomzan /js/app.js
@@ -20,11 +20,16 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+      <button onclick = "addToCart(${product.id},${product.price})"
+      id = "addToCart-btn"
+      class = "buy-now btn btn-success" > add to cart </button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
+      
     document.getElementById("all-products").appendChild(div);
+  
   }
+  
 };
 let count = 0;
 const addToCart = (id, price) => {
@@ -51,7 +56,7 @@ const updatePrice = (id, value) => {
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = Math.round(value)
 };
 
 // update delivery charge and total Tax
@@ -59,16 +64,17 @@ const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
   if (priceConverted > 200) {
     setInnerText("delivery-charge", 30);
-    setInnerText("total-tax", priceConverted * 0.20);
+    setInnerText("total-tax", priceConverted * 0.2);
   }
   if (priceConverted > 400) {
     setInnerText("delivery-charge", 50);
-    setInnerText("total-tax", priceConverted * 0.30);
+    setInnerText("total-tax", priceConverted * 0.3);
   }
   if (priceConverted > 500) {
     setInnerText("delivery-charge", 60);
-    setInnerText("total-tax", priceConverted * 0.40);
+    setInnerText("total-tax", priceConverted * 0.4);
   }
+  updateTotal()
 };
 
 //grandTotal update function
@@ -78,3 +84,4 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
 };
+
